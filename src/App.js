644733473f48a69d224a68b0cff96d34a3707c37
Y39WebDeveloper <y39freelancer@gmail.com>
2023-02-08 15:17:53 +0100

    done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Footer } from './Section';
 function App() {
   return (
     <>
-      <Router>
+      <Router basename='/Portfolio'>
         <Navbar />
           <Routes>
             <Route path='/' element={<Home/>} />
